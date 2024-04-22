2000218991e0fa58583e3b752f460c2a547ec448
ldduc123 <ducld0510@gmail.com>
2024-04-22 21:48:55 +0700

    Add Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Header from "./components/Header";
 
 function App() {
-  
-
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+
+
+        <Route path="*" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
